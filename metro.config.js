53af121b18a3976e1444620c3cf34849b107fa33
Holmesen <1315654996@qq.com>
2025-05-10 16:25:05 +0800

    fix: 修复 Supabase 和 Expo SDK 53 集成问题

--- metro.config.js
+++ metro.config.js
@@ -4,7 +4,7 @@ const { getDefaultConfig } = require('expo/metro-config');
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
 
-// Supabase fix
+// Supabase fix (https://github.com/expo/expo/issues/36375) (https://github.com/supabase/realtime-js/issues/415)
 config.resolver.unstable_conditionNames = ['browser'];
 config.resolver.unstable_enablePackageExports = false;
 
