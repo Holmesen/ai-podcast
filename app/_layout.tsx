519db26fa39b3cbde284da2526c1cc4473cb9657
Holmesen <1315654996@qq.com>
2025-05-14 14:33:43 +0800

    feat: 更新应用配置，修改图标路径，添加 EAS 配置文件，更新依赖项，优化布局结构

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -122,7 +122,6 @@ export default function RootLayout() {
           />
           <Stack.Screen name="(auth)" />
           <Stack.Screen name="(tabs)" />
-          <Stack.Screen name="(podcast)" />
         </Stack>
       </SafeAreaProvider>
     </ErrorBoundary>
